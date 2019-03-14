b3bb0458829003f8e4d056aec59485c0a93d65c8
sohee <shfksthgml@naver.com>
2019-03-14 10:18:23 +0900

    <MainActivity> 짝수에만 하트 보이게 하기

--- homework03/app/src/main/java/com/androidstudy/homework03/MainActivity.kt
+++ homework03/app/src/main/java/com/androidstudy/homework03/MainActivity.kt
@@ -4,35 +4,33 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.widget.GridLayout
+import com.androidstudy.homework03.R.id.rv_main_act_grid_view
 import com.androidstudy.homework03.adapter.GridRecyclerViewAdapter
 import com.androidstudy.homework03.data.MyItemData
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-lateinit var gridRecyclerViewAdapter: GridRecyclerViewAdapter
+    lateinit var gridRecyclerViewAdapter: GridRecyclerViewAdapter
+    var dataList: ArrayList<MyItemData> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-            //makeData()
+        makeData()
         setRecyclerView()
     }
 
-    fun makeData(){
+    fun makeData() {
+        for(i in 1..30){
+            if(i%2 ==0) dataList.add(MyItemData(i,true))
+            else dataList.add(MyItemData(i,false))
+        }
     }
 
-    fun setRecyclerView(){
-        var dataList :ArrayList<MyItemData> = ArrayList()
-        dataList.add(MyItemData(1,false))
-        dataList.add(MyItemData(1,false))
-        dataList.add(MyItemData(1,false))
-        dataList.add(MyItemData(1,false))
-        dataList.add(MyItemData(1,false))
-        dataList.add(MyItemData(1,false))
-        dataList.add(MyItemData(1,false))
-        dataList.add(MyItemData(1,false))
-        gridRecyclerViewAdapter = GridRecyclerViewAdapter(this,dataList)
-        rv_main_act_grid_view.adapter= gridRecyclerViewAdapter
-        rv_main_act_grid_view.layoutManager = GridLayoutManager(this,3)
+    fun setRecyclerView() {
+        gridRecyclerViewAdapter = GridRecyclerViewAdapter(this, dataList)
+        rv_main_act_grid_view.adapter = gridRecyclerViewAdapter
+        rv_main_act_grid_view.layoutManager = GridLayoutManager(this, 3)
     }
 
 }
