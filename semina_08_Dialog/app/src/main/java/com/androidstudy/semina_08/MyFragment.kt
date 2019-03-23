eed8188b34b876a853ed1295e623d4fd49464ff0
sohee <shfksthgml@naver.com>
2019-03-23 13:21:43 +0900

    <MyFragment> onCreateView 에 레이아웃 inflate

--- semina_08_Dialog/app/src/main/java/com/androidstudy/semina_08/MyFragment.kt
+++ semina_08_Dialog/app/src/main/java/com/androidstudy/semina_08/MyFragment.kt
@@ -44,6 +44,8 @@ class MyFragment : Fragment(){
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_my,container,false)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
